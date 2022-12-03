fe45cb5639a3bee2ed9b67c1faeeab990c2c7092
ShauryaMadukuri <shauryamadukuri@gmail.com>
2022-12-04 01:53:07 +0530

    added the generator block

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,46 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import {useState} from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Unleash Your Inner Poet</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Unlock Creative Rhymes and Verses Instantly</h2>
           </div>
         </div>
+        {/* Add this code here*/}
+        
+        <div className="prompt-container">
+          <textarea className='prompt-box'
+            placeholder="Enter a prompt here..."
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          {/* button */}
+          <div className="prompt-buttons">
+              <a className="generate-button" onClick={null}>
+                <div className="generate">
+                  <p>Generate</p>
+                </div>
+              </a>
+          </div>
+        </div>
+
+        
+
+        
       </div>
       <div className="badge-container grow">
         <a
